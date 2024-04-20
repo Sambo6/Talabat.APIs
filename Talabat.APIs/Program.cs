42411d334e1ade5d79e8792838149f5523b4a05c
MUhammed_AbdUllah <67861134+Sambo6@users.noreply.github.com>
2024-04-20 05:38:33 +0200

    Part 13 Handling Not Found Endpoint

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -118,6 +118,7 @@ namespace Talabat.APIs
                 app.UseSwaggerUI();
             }
 
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
