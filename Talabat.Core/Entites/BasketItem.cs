f41a47b2fba388b2a998908398c10c36e8f043c7
MUhammed_AbdUllah <67861134+Sambo6@users.noreply.github.com>
2024-04-25 19:16:29 +0200

    Part 01 Basket Module Entities

---
+++ Talabat.Core/Entites/BasketItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Talabat.Core.Entities
+{
+    public class BasketItem
+    {
+        //Id OF PRODUCT
+        public int Id { get; set; }
+        public string ProductName { get; set; } = null!;
+        public string PictureUrl { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public string Brand { get; set; } = null!;
+        public int Quantity { get; set; }
+
+
+    }
+}
