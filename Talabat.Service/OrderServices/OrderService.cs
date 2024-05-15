d20fe70e6458aa3d62425a374d5ddf99a155162c
MUhammed_AbdUllah <67861134+Sambo6@users.noreply.github.com>
2024-05-15 04:04:24 +0300

    000

--- Talabat.Service/OrderServices/OrderService.cs
+++ Talabat.Service/OrderServices/OrderService.cs
@@ -1,18 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Talabat.Core.Entities;
 using Talabat.Core.Entities.Order_Aggregate;
+using Talabat.Core.Repositories.Contract;
 using Talabat.Core.Services.Contract;
 
 namespace Talabat.Application.OrderServices
 {
 	public class OrderService : IOrderService
 	{
-		public Task<Order> CreateOrder(string buyerEmail, string basketId, string deliveryMethodId, Address shippingAddress)
+		
+
+		
+		private readonly IBasketRepository _basketRepo;
+		private readonly IGenericRepository<Product>_productRepo;
+		private readonly IGenericRepository<DeliveryMethod> _deliveryMethodRepo;
+		private readonly IGenericRepository<Order> _orderRepo;
+		public OrderService(IBasketRepository basketRepo,
+							IGenericRepository<Product> productRepo,
+							IGenericRepository<DeliveryMethod> deliveryMethodRepo,
+							IGenericRepository<Order> orderRepo) 
 		{
-			throw new NotImplementedException();
+			_basketRepo = basketRepo;
+			_productRepo = productRepo;
+			_deliveryMethodRepo = deliveryMethodRepo;
+			_orderRepo = orderRepo;
+		}
+
+		public async Task<Order> CreateOrderAsync(string buyerEmail, string basketId, int deliveryMethodId, Address shippingAddress)
+		{
+			// 1.Get Basket From BasketsRepo
+			var basket = await _basketRepo.GetBasketAsync(basketId);
+
+			// 2. Get Selected Items at Basket From ProductsRepo
+
+			var orderItems = new List<OrderItem>();
+			if (basket?.Items?.Count > 0 )
+			{
+				foreach ( var item in basket.Items )
+				{
+					var product = await _productRepo.GetAsync(item.Id);
+					var productItemOrdered = new ProductItemOrder(product.Id, product.Name, product.PictureUrl);
+					var orderItem = new OrderItem(productItemOrdered, product.Price, item.Quantity);
+					orderItems.Add(orderItem);
+				}
+			}
+
+			// 3. Calculate SubTotal
+
+			var subtotal = orderItems.Sum(item => item.Price * item.Quantity);
+
+			// 4. Get Delivery Method From DeliveryMethodsRepo
+
+			//var deliveryMethod = await _deliveryMethodRepo.GetAsync(deliveryMethodId);
+
+
+			// 5. Create Order
+
+			var order = new Order(
+
+				buyerEmail: buyerEmail,
+				shippingAddress: shippingAddress,
+				deliveryMethodId: deliveryMethodId,
+				items: orderItems,
+				subtotal: subtotal
+
+				);
+			_orderRepo.Add(order);
+
+			// 6. Save To Database [TODO]
+
 		}
 
 		public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
