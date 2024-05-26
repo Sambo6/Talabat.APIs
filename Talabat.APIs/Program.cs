d5c36726dd24198b9b54eaba4e713088f91f1e0a
MUhammed_AbdUllah <67861134+Sambo6@users.noreply.github.com>
2024-05-26 22:03:04 +0300

    #Part 06 Test Payment Cycle Without Webhook

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -11,11 +11,11 @@ using Talabat.Repository.Data;
 namespace Talabat.APIs
 {
     public class Program
-	{
-		//Entry point
-		public static async Task Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+    {
+        //Entry point
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
             // Add services to the container.
 
             #region Configure Services
@@ -48,68 +48,74 @@ namespace Talabat.APIs
                 return ConnectionMultiplexer.Connect(connection);
             });
 
+            builder.Services.AddAuthServices(builder.Configuration);
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyPolicy", policyOptions =>
+                {
+                    policyOptions.AllowAnyHeader().AllowAnyMethod().WithOrigins(builder.Configuration["FrontBaseUrl"]);
+                });
+            });
+
+            #endregion
+
+            var app = builder.Build();
+
+            #region Update Database Dynamic
+
+            using var scope = app.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
 
 
+            var _dbContext = services.GetRequiredService<StoreContext>(); // ask clr for creating object from DbContext Explicitly
+            var _identityDbContext = services.GetRequiredService<ApplicationIdentityDbContext>();
 
-			builder.Services.AddAuthServices(builder.Configuration);
-			
-			#endregion
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
 
-			var app = builder.Build();
+            try
+            {
+                await _dbContext.Database.MigrateAsync();
+                await StoreContextSeeding.SeedAsync(_dbContext);
 
-			#region Update Database Dynamic
+                await _identityDbContext.Database.MigrateAsync();
 
-						using var scope = app.Services.CreateScope();
-
-			var services = scope.ServiceProvider;
-
-
-			var _dbContext = services.GetRequiredService<StoreContext>(); // ask clr for creating object from DbContext Explicitly
-			var _identityDbContext = services.GetRequiredService<ApplicationIdentityDbContext>();
-
-			var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-
-			try
-			{
-				await _dbContext.Database.MigrateAsync();
-				await StoreContextSeeding.SeedAsync(_dbContext);
-
-				await _identityDbContext.Database.MigrateAsync();
-
-				var _userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-				await ApplicationIdentityContextSeed.SeedUsersAsync(_userManager);
-			}
-			catch (Exception ex)
-			{
-				var logger = loggerFactory.CreateLogger<Program>();
-				logger.LogError(ex, "an error has been occured during apply the migration");
-			}
+                var _userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                await ApplicationIdentityContextSeed.SeedUsersAsync(_userManager);
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "an error has been occured during apply the migration");
+            }
             #endregion
 
             #region Kestrel Middlewares
             // Configure the HTTP request pipeline.
 
             ///1. ByConvention Based
-			
+
             app.UseMiddleware<ExceptionMiddleware>();
 
 
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwaggerMiddleware();
-			}
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwaggerMiddleware();
+            }
 
-			app.UseStatusCodePagesWithReExecute("/errors/{0}");
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
-			app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
-			app.UseStaticFiles();
+            app.UseStaticFiles();
 
-			app.MapControllers();
+            app.UseCors("MyPolicy");
+            app.MapControllers();
 
-			#endregion
+            #endregion
 
-			app.Run();
-		}
-	}
+            app.Run();
+        }
+    }
 }
