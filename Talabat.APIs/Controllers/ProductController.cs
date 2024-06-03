380d935e23a98aaa08abad4d9ccd204e784bb56f
MUhammed_AbdUllah <67861134+Sambo6@users.noreply.github.com>
2024-06-03 23:59:45 +0300

    Part 04 Debugging Code and Check Performance [Done]

--- Talabat.APIs/Controllers/ProductController.cs
+++ Talabat.APIs/Controllers/ProductController.cs
@@ -14,7 +14,6 @@ namespace Talabat.APIs.Controllers
     {
         private readonly IProductService _productService;
         private readonly IMapper _mapper;
-
         public ProductsController(
             IProductService productService,
             IMapper mapper)
@@ -22,9 +21,13 @@ namespace Talabat.APIs.Controllers
             _productService = productService;
             _mapper = mapper;
         }
+
+
         [Cached(800)]
         [HttpGet]
         //[Authorize()]
+
+        #region GetProducts EndPoint
         public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery] ProductSpecParams specParams)
         {
             var products = await _productService.GetProductsAsync(specParams);
@@ -34,11 +37,16 @@ namespace Talabat.APIs.Controllers
             var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
 
             return Ok(new Pagination<ProductToReturnDto>(specParams.PageIndex, specParams.PageSize, count, data));
-        }
+        } 
+        #endregion
 
+
+        [Cached(800)]
         [ProducesResponseType(typeof(ProductToReturnDto), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         [HttpGet("{id}")]
+
+        #region GetProduct EndPoint
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var product = await _productService.GetProductAsync(id);
@@ -47,22 +55,34 @@ namespace Talabat.APIs.Controllers
                 return NotFound(new ApiResponse(404)); // 404
 
             return Ok(_mapper.Map<Product, ProductToReturnDto>(product)); // 200
-        }
+        } 
+        #endregion
 
+
+        [Cached(800)]
         [HttpGet("brands")]
+
+        #region GetBrands EndPoint
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetBrands()
         {
             var brands = await _productService.GetBrandsAsync();
 
             return Ok(brands);
-        }
+        } 
+        #endregion
 
+
+        [Cached(800)]
         [HttpGet("categories")]
+
+        #region GetCategories EndPoint
         public async Task<ActionResult<IReadOnlyList<ProductCategory>>> GetCategories()
         {
             var categories = await _productService.GetCategoriesAsync();
 
             return Ok(categories);
-        }
+        } 
+        #endregion
+
     }
 }
